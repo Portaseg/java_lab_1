acb60c7f98d464e4be31c57333ccfc7888347535
PPodwojski <pietrolsek@gmail.com>
2022-01-18 21:45:30 +0100

    zrobione ćwiczenie 4

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,21 @@ public class Main {
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
 
+        boolean a = true;
+        boolean b = false;
 
+        System.out.println("(a||b): " + (a||b));
+        System.out.println("(a&&b): " + (a&&b));
+        System.out.println("(a!=b): " + (a!=b));
+
+        int c=2;
+        int d=4;
+        int e=10;
+
+        // rzutowanie zmiennej calkowitej na rzeczywista
+        System.out.println("wynik: " + e / (double)d);
+
+        System.out.println("(c<d): " + (c<d));
+        System.out.println("((e&&e)||(e>e)): " + ((a&&b)||(e>e)));
     }
 }
