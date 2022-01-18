8ade569345dcb9ee317bfecab32dbc374637be23
PPodwojski <pietrolsek@gmail.com>
2022-01-18 20:49:43 +0100

    zrobione ćwiczenie 2

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,20 @@ public class Main {
         /* ćw.2 */
 
         /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
+        byte zmiennabyte = 127;
+        short zmiennashort = 32767;
+        int zmiennaint = 110122;
+        long zmiennalong = 111000111000L;
+        float zmiennafloat = 20.12f;
+        double zmiennadouble = 123456.1234;
+        boolean zmiennaboolean = true;
+        char zmiennachar = 'a';
+
+        int zmienna1=128;
+        int a, b=4, c=2;
+        int wynik = b/c;
+        System.out.println("wynik = " + wynik);
+
 
         /*
         *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
@@ -17,6 +31,40 @@ public class Main {
         *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
         *  */
 
+        int liczbaA = 4;
+        int liczbaB = 2;
+        double liczbaX = 2.1;
+        double liczbaY = 1.2;
 
+        int dodawanie = liczbaA + liczbaB;
+        System.out.println("dodawanie = " + dodawanie);
+
+        int odejmowanie = liczbaA - liczbaB;
+        System.out.println("odejmowanie = " + odejmowanie);
+
+        int mnozenie = liczbaA * liczbaB;
+        System.out.println("mnożenie = " + mnozenie);
+
+        int dzielenie = liczbaA / liczbaB;
+        System.out.println("dzielenie = " + dzielenie);
+
+        int modulo = liczbaA % liczbaB;
+        System.out.println("modulo = " + modulo);
+
+
+        double Add = liczbaY + liczbaX;
+        System.out.println("Add = " + Add);
+
+        double Sub = liczbaX - liczbaY;
+        System.out.println("Sub = " + Sub);
+
+        double multiply = liczbaY * liczbaX;
+        System.out.println("multiply = " + multiply);
+
+        double divide = liczbaX / liczbaY;
+        System.out.println("divide = " + divide);
+
+        double modulox = liczbaX % liczbaY;
+        System.out.println("modulox = " + modulox);
     }
 }
