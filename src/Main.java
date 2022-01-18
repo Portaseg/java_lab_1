8d3eba5d3177e3c12ec9dda812b0086c89542fc7
PPodwojski <pietrolsek@gmail.com>
2022-01-18 22:55:20 +0100

    zrobione ćwiczenie 6

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,14 @@ public class Main {
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
 
+        int[] tablica = new int[5];
 
+        for (int i = 0; i < tablica.length; i++) {
+            tablica[i] = i + 10;
+            tablica[2] = 51;
+        }
+        for (int i = 0; i < tablica.length; i++) {
+            System.out.println("Element " + i + ", " + "wartosc: " + tablica[i]);
+        }
     }
 }
